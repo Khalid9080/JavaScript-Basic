2bb1a9efb21c2d843770bbd987be077cdc47eebf
Khalid9080 <103997135+Khalid9080@users.noreply.github.com>
2024-09-08 23:07:55 +0600

    Add files via upload

---
+++ JavaScript/Practice-Problem-Part-1/Date.js
@@ -0,0 +1,12 @@
+
+
+
+
+const today = new Date(2024,0,26);
+console.log(today);
+today.setMonth(10);
+console.log(today.toLocaleDateString('en-GB'));
+
+
+const date = new Date('2062-10-09')
+console.log(date);
